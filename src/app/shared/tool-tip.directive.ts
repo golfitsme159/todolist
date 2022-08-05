0972f5a46aa72ea20d6daaef5a0542dd1410c8de
golfitsme <goltisme@gmail.com>
2022-08-06 01:07:23 +0700

    update

---
+++ src/app/shared/tool-tip.directive.ts
@@ -0,0 +1,18 @@
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
+import tippy from 'tippy.js';
+
+@Directive({
+  selector: '[appToolTip]',
+})
+export class ToolTipDirective implements AfterViewInit {
+  @Input('appToolTip')
+  tooltipContent!: string;
+
+  constructor(private elRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    tippy(this.elRef.nativeElement, {
+      content: this.tooltipContent,
+    });
+  }
+}
---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Todo } from '../shared/todo.model';
+import tippy from 'tippy.js';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css'],
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo!: Todo;
+  @Output() todoClicked: EventEmitter<void> = new EventEmitter();
+  @Output() editClicked: EventEmitter<void> = new EventEmitter();
+  @Output() deleteClicked: EventEmitter<void> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onTodoClicked() {
+    this.todoClicked.emit();
+  }
+
+  onEditClicked() {
+    this.editClicked.emit();
+  }
+
+  onDeleteClicked() {
+    this.deleteClicked.emit();
+  }
+}
